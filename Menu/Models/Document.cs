437b42a98717a836206343ddc1aa2490ab63aa0c
ruban <JY-RedWood@JY-RUBY>
2024-06-26 14:45:33 +0800

    something

---
+++ Menu/Models/Document.cs
@@ -0,0 +1,14 @@
+﻿namespace Menu.Models
+{
+    public class Document
+    {
+        // This section for Document
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        // This section for connecting Document to an User
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
